c5ebc8460abc3f87cd6d6d9218672a50239280de
Aneeta Sharma <get.aneeta@gmail.com>
2020-02-22 14:48:24 +0545

    Add and implement vuetify library.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,25 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      color="deep-purple accent-4"
+      dense
+      dark
+    >
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-content>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
@@ -17,6 +35,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
