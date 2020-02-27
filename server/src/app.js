b98e7460b0c64305e1b9579b9d44deee06b84431
Aneeta Sharma <get.aneeta@gmail.com>
2020-02-27 23:16:12 +0545

    Add a real api for listing and adding users.

--- server/src/app.js
+++ server/src/app.js
@@ -3,152 +3,40 @@ const bodyParser = require('body-parser');
 const querystring = require('querystring');
 const cors = require('cors');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const fs = require('fs');
+const config = require('./config');
 
 const app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
+mongoose.connect(config.DB, 
+	{ 
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	}, function() {
+  console.log('Connection has been made');
+})
+.catch(err => {
+  console.error('App starting error:', err.stack);
+  process.exit(1);
+});
+
+// Include controllers
+fs.readdirSync(__dirname + "/controllers").forEach(function (file) {
+  if(file.substr(-3) == ".js") {
+    const route = require("./controllers/" + file)
+    route.controller(app)
+  }
+})
+
 app.get('/', (req, res) => {
 	res.send(
 		[{
 			message: "All Good from Backend!",
 			}]
-	)
-})
-
-app.get('/people', (req, res) => {
-	let page = req.query.page;
-	let response = {};
-	if (!page || page == 1) {
-		response = {
-			meta: {
-				total: 16,
-				page: 1,
-				per_page: 7
-			},
-			people: [
-				{
-					name: 'Anita Sharma',
-					location: 'Kathmandu',
-					status: 'Discarded',
-					email: 'get.aneeta@gmail.com'
-				},
-				{
-					name: 'Nelson Smith',
-					location: 'Waterford',
-					status: 'Employed',
-					email: 'nelson.smith@example.com'
-				},
-				{
-					name: 'Lynn Foster',
-					location: 'Waterford',
-					status: 'Employed',
-					email: 'lynn.foster@example.com'
-				},
-				{
-					name: 'Darryl Simpson',
-					location: '3706 Cackson St',
-					status: 'Employed',
-					email: 'darryl.simpson@example.com'
-				},
-				{
-					name: 'Walter Sutton',
-					location: '6649 Robinson Rd',
-					status: 'Discarded',
-					email: 'walter.sutton@example.com'
-				},
-				{
-					name: 'Mabel Carter',
-					location: '732 W Gray St',
-					status: 'Employed',
-					email: 'mabel.carter@example.com'
-				},
-				{
-					name: 'Sally Richards',
-					location: '4668 Pockrus Page Rd',
-					status: 'Employed',
-					email: 'sally.richards@example.com'
-				}
-			]
-		}
-	} else if (page == 2) {
-		response = {
-			meta: {
-				total: 16,
-				page: 2,
-				per_page: 7
-			},
-			people: [
-				{
-					name: 'Anita Sharma',
-					location: 'Kathmandu',
-					status: 'Discarded',
-					email: 'get.aneeta@gmail.com'
-				},
-				{
-					name: 'Nelson Smith',
-					location: 'Waterford',
-					status: 'Employed',
-					email: 'nelson.smith@example.com'
-				},
-				{
-					name: 'Lynn Foster',
-					location: 'Waterford',
-					status: 'Employed',
-					email: 'lynn.foster@example.com'
-				},
-				{
-					name: 'Darryl Simpson',
-					location: '3706 Cackson St',
-					status: 'Employed',
-					email: 'darryl.simpson@example.com'
-				},
-				{
-					name: 'Walter Sutton',
-					location: '6649 Robinson Rd',
-					status: 'Discarded',
-					email: 'walter.sutton@example.com'
-				},
-				{
-					name: 'Mabel Carter',
-					location: '732 W Gray St',
-					status: 'Employed',
-					email: 'mabel.carter@example.com'
-				},
-				{
-					name: 'Darrell Sanchez',
-					location: '4376 First Street',
-					status: 'Employed',
-					email: 'darrell.sanchez@example.com'
-				}
-			]
-		};
-	} else if (page == 3) {
-		response = {
-			meta: {
-				total: 16,
-				page: 3,
-				per_page: 7
-			},
-			people: [
-				{
-					name: 'Mabel Carter',
-					location: '732 W Gray St',
-					status: 'Employed',
-					email: 'mabel.carter@example.com'
-				},
-				{
-					name: 'Darrell Sanchez',
-					location: '4376 First Street',
-					status: 'Employed',
-					email: 'darrell.sanchez@example.com'
-				}
-			]
-		};
-	}
-	res.send(
-		response
 	)
 })
 
