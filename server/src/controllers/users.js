0964dcfd0d7a47b1b8b86234d6abb6742eb3c001
Aneeta Sharma <get.aneeta@gmail.com>
2020-02-29 23:43:50 +0545

    Remove get api as search api will be enough

--- server/src/controllers/users.js
+++ server/src/controllers/users.js
@@ -1,32 +1,6 @@
 var User = require("../models/User");
 
 module.exports.controller = (app) => {
-  // fetch all users
-  app.get("/people", function(req, res) {
-    let page = parseInt(req.query.page) || 1;
-    let perPage = 7;
-    let response = {};
-    User.find({}, 'name location email status dob contact picture', { skip: perPage * (page-1), limit: perPage, sort: { '_id': -1 } }, function (error, users) {
-      if (error) { console.log(error); }
-      
-      User.countDocuments().exec((error, count) => {
-        if (error) {
-          return res.json(count_error);
-        }
-
-        response = {
-          meta: {
-            total: count,
-            page: page,
-            per_page: perPage
-          },
-          people: users
-        }
-        res.send(response)
-      });
-    })
-  })
-
   // add a new user
   app.post('/users', (req, res) => {
     const user = new User({
