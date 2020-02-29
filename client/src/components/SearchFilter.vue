5b90cdd85546452de6e732791e352ea9692050ab
Aneeta Sharma <get.aneeta@gmail.com>
2020-02-29 21:24:56 +0545

    Search using enter key

--- client/src/components/SearchFilter.vue
+++ client/src/components/SearchFilter.vue
@@ -1,6 +1,6 @@
 <template>
   <b-row class="mt-3 mb-3">
-    <b-col><b-form-input id="search" v-model="searchText" placeholder="Search users" trim></b-form-input></b-col>
+    <b-col><b-form-input id="search" v-on:keyup="onKeyUp" v-model="searchText" placeholder="Search users" trim></b-form-input></b-col>
     <b-col cols="12" md="auto"><b-form-select v-model="filter" :options="filterOptions"></b-form-select></b-col>
     <b-col col lg="1"><b-button variant="success" @click="onChange()">Filter</b-button></b-col>
   </b-row>
@@ -25,6 +25,11 @@ export default {
     };
   },
   methods: {
+    onKeyUp: function(e) {
+      if (e.keyCode === 13) {
+        this.onChange();
+      } 
+    },
     async onChange() {
       const params = {};
       params[this.filter || 'any'] = this.searchText;
