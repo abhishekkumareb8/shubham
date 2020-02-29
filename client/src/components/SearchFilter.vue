720cda731fdc8839c4a987cc5a21898210cf915f
Aneeta Sharma <get.aneeta@gmail.com>
2020-02-29 21:28:19 +0545

    Update method name

--- client/src/components/SearchFilter.vue
+++ client/src/components/SearchFilter.vue
@@ -1,6 +1,6 @@
 <template>
   <b-row class="mt-3 mb-3">
-    <b-col><b-form-input id="search" v-on:keyup="onKeyUp" v-model="searchText" placeholder="Search users" trim></b-form-input></b-col>
+    <b-col><b-form-input id="search" v-on:keyup="onSearchKeyUp" v-model="searchText" placeholder="Search users" trim></b-form-input></b-col>
     <b-col cols="12" md="auto"><b-form-select v-model="filter" :options="filterOptions"></b-form-select></b-col>
     <b-col col lg="1"><b-button variant="success" @click="onChange()">Filter</b-button></b-col>
   </b-row>
@@ -25,10 +25,10 @@ export default {
     };
   },
   methods: {
-    onKeyUp: function(e) {
+    onSearchKeyUp(e) {
       if (e.keyCode === 13) {
         this.onChange();
-      } 
+      }
     },
     async onChange() {
       const params = {};
