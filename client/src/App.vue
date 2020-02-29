61b60ed3dbed7c33a08800ab42ae12a55b8540b0
Aneeta Sharma <get.aneeta@gmail.com>
2020-02-29 21:10:40 +0545

    Refactor navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,10 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand-lg navbar-dark header pl-5">
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a class="nav-item nav-link active" href="/">People <span class="sr-only">(current)</span></a>
-        </div>
-      </div>
-    </nav>
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="primary">
+        <b-navbar-brand href="#">People</b-navbar-brand>
+      </b-navbar>
+    </div>
     <b-container fluid="sm">
       <router-view/>
     </b-container>
